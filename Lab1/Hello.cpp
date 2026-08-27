05d7b1e78bea01da8e1fc6ec66adcf73ace3848c
ArterraS4 <arterra44@gmail.com>
2026-08-27 01:10:41 -0700

    Update Hello.cpp

--- Lab1/Hello.cpp
+++ Lab1/Hello.cpp
@@ -1,13 +1,13 @@
-
-
-
+/***
+ * Lab 1 introduction to Object-Oriented Programming and basic output.
+ ***/
 
 #include <iostream>
 #include <cstdlib>
 using namespace std;
 
 int main() {
-  cout << "Hello World" << endl;
+  cout << "Hello World!" << endl;
   printf("Hello World Again!\n");
   return 0;
 }
